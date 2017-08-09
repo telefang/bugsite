11d89adeabbe6aad3672fa2c2f01e392a4988b84
David Wendt <dcrkid@yahoo.com>
2017-08-08 20:28:34 -0400

    Add code for parsing the string table and character map.

---
+++ utilities/BugTools/bvm/strings.py
@@ -0,0 +1,41 @@
+def parse_stringtbl(infile):
+    """Parse a list of string equates for use by the .bvm assembler."""
+
+    known_equates = {}
+
+    for line in infile:
+        sym, decl = line.split("\t")
+
+        decl = decl.strip()
+        decl = decl[1:-1]
+
+        known_equates[sym] = decl
+
+    return known_equates
+
+def parse_charmap(infile):
+    """Parse the character mapping for this ROM.
+
+    Returns a function which can be used as a .bvm string encoder."""
+
+    encoder_mapping = {}
+
+    def encoder(instr):
+        output = []
+
+        for byte in instr:
+            output.append(encoder_mapping[byte])
+
+        return b"".join(output)
+
+    #Actual parsing starts here
+    for encode_slot, line in enumerate(infile):
+        decoded_char = line[:-1]
+
+        if len(decoded_char) != 1:
+            #We don't (yet) support multicharacter ligatures
+            continue
+
+        encoder_mapping[decoded_char] = bytes([encode_slot])
+
+    return encoder
