9d21008f15c7c567f4b49e56d176eed58e114c68
David Wendt <dcrkid@yahoo.com>
2017-08-25 22:22:59 -0400

    Add stringtable compiler

--- utilities/BugTools/montable/command.py
+++ utilities/BugTools/montable/command.py
@@ -1,4 +1,6 @@
 from BugTools.montable.extract import extract_bugnames_from_block
+from BugTools.montable.parser import parse_names_list
+from BugTools.montable.stringbuilder import build_string_object
 from BugTools.bvm.strings import parse_charmap
 
 import argparse, sys, csv
@@ -25,3 +27,23 @@ def montable_extract():
         for row in tabledata:
             encoded_row = [cell.encode('utf-8') for cell in row]
             csvwriter.writerow(row)
+
+def montable_compile():
+    parser = argparse.ArgumentParser(description='Compiles name data into the name slots of the attribute tables in Network Adventure Bugsite. Produces an RGBDS compatible object file.')
+
+    parser.add_argument('infile', metavar='file.csv', type=str, help='The file to pull strings from.')
+    parser.add_argument('charmap', metavar='charmap.bin', type=str, help='Character mapping to decode with.')
+    parser.add_argument('output', metavar='file.atbl.o', type=str, help='Where to save the resulting object file.')
+    parser.add_argument('--language', type=str, default=u"Japanese")
+
+    args = parser.parse_args()
+
+    with open(args.charmap, encoding="utf-8") as mapfile:
+        strenc, strdec = parse_charmap(mapfile)
+
+    with open(args.infile, encoding="utf-8") as infile:
+        strlist = parse_names_list(infile, args.language)
+        stringobj = build_string_object(strlist, strenc)
+
+    with open(args.output, "wb") as rgbfile:
+        rgbfile.write(stringobj.bytes)
