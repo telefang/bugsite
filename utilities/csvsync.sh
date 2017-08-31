bb0e770ca17f805781110ad460a4b7c6241d6ff4
David Wendt <dcrkid@yahoo.com>
2017-08-30 22:04:15 -0400

    Add a script that pulls translated string data from GDrive.

---
+++ utilities/csvsync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget --no-check-certificate -O script/bugvm_strings_story.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=0
+wget --no-check-certificate -O script/bugvm_strings_npc.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=673537365
+wget --no-check-certificate -O script/bugvm_strings_system.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=454319826
+wget --no-check-certificate -O versions/alpha/script/monsters.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=569821359
+wget --no-check-certificate -O script/moves.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=793073010
+wget --no-check-certificate -O script/chips.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=6432762
+wget --no-check-certificate -O script/keyitems.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=186923876
+wget --no-check-certificate -O versions/alpha/script/encounters.csv https://docs.google.com/spreadsheet/ccc?key=1tlWxI2okX3H596DxpfO2g7CGqNvA-WCtOu0Pw4RCDws\&output=csv\&gid=124029920
