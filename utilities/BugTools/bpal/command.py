e98fff2f2d5ee2378be353aafe7f410777ffe93e
David Wendt <dcrkid@yahoo.com>
2017-10-15 19:01:15 -0400

    Add code for compiling palette source (bpal)

---
+++ utilities/BugTools/bpal/command.py
@@ -0,0 +1,32 @@
+from BugTools.bpal.parser import bpal_grammar, PaletteDataVisitor
+from BugTools.bpal.passes import palette_encode
+
+import argparse, sys
+
+y = """
+        #012345, #67890a, #bcdef0, #123456
+    P_MainScript_Theme000: #012345, #67890a, #bcdef0, #123456
+    P_MainScript_Theme001: #012345, #67890a, #bcdef0, #123456
+    """
+
+#print(bpal_grammar.parse(y))
+print(PaletteDataVisitor().visit(bpal_grammar.parse(y)))
+
+def bpalasm():
+    parser = argparse.ArgumentParser(description="An assembler and source format for palette data in KAZe developed Game Boy games.")
+    
+    parser.add_argument("infile", metavar="file.bpal", type=str, help='The palette data to assemble.')
+    parser.add_argument('output', metavar='file.palette.bin', type=str, help='Where to save the assembled palette colors.')
+    
+    args = parser.parse_args()
+    
+    with open(args.infile) as srcfile:
+        src = srcfile.read()
+        tree = bpal_grammar.parse(src + "\n") #Add a newline. Our grammar doesn't like files without ending newlines.
+        
+        mp = PaletteDataVisitor().visit(tree)
+        
+        pdata = palette_encode(mp)
+        
+        with open(args.output, "wb") as outfile:
+            outfile.write(pdata)
---
+++ utilities/BugTools/bpal/parser.py
@@ -0,0 +1,102 @@
+from parsimonious.grammar import Grammar
+from parsimonious.nodes import NodeVisitor, Node
+
+from BugTools.util import flatten
+
+bpal_grammar = Grammar(
+    """
+    lines = _ line*
+    line = (palette eol) / (label? palette? eol) / eol
+    palette = color color color color _
+    color = ~"#" hexit hexit hexit ~","? _
+    
+    label = ~"[a-zA-Z_][a-zA-Z_0-9]*" ":" _
+    hexit = ~"[A-F0-9]"i ~"[A-F0-9]"i
+    
+    eol = _? newline _?
+    newline = ~"[\\r\\n]"
+    
+    _ = meaninglessness*
+    meaninglessness = whitespace / comment
+    comment = ~";[^\\r\\n]*"
+    whitespace = ~"[ \t]+"
+    """
+)
+
+class Label(object):
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def __repr__(self):
+        return "Label(%s)" % (repr(self.symbol),)
+
+class PaletteEntry(object):
+    def __init__(self, colors):
+        self.colors = colors
+    
+    def __repr__(self):
+        return "PaletteEntry(%s)" % (repr(self.colors), )
+
+class Color(object):
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+    
+    def __repr__(self):
+        return "Color(%s, %s, %s)" % (repr(self.r), repr(self.g), repr(self.b))
+
+class PaletteDataVisitor(NodeVisitor):
+    """Convert a parsed .bpal syntax tree into a list of palettes."""
+    
+    def generic_visit(self, node, children):
+        """Unwrap nodes we don't care about"""
+        return children or node
+    
+    def visit_hexit(self, node, children):
+        idat = 0
+        
+        for child in children:
+            idat = (idat << 4) + int(child.text, 16)
+        
+        return idat
+    
+    def visit_color(self, node, children):
+        return Color(children[1], children[2], children[3])
+    
+    def visit_palette(self, node, children):
+        return PaletteEntry([children[0], children[1], children[2], children[3]])
+    
+    def visit_label(self, node, children):
+        symbol, useless_colon, _ = children
+
+        return Label(symbol.text)
+
+    def visit_line(self, node, children):
+        output = []
+
+        for child in flatten(children):
+            if type(child) == Node or type(child) == str and child == "":
+                continue
+
+            output.append(child)
+
+        return output
+
+    def visit_lines(self, node, children):
+        output = []
+
+        for child in flatten(children):
+            try:
+                if child.text.strip() == "":
+                    continue
+            except AttributeError:
+                try:
+                    if child.strip() == "":
+                        continue
+                except AttributeError:
+                    pass
+
+            output.append(child)
+
+        return output
