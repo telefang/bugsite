7f1477b0f64f0e8916cae590c65b598d5249731e
David Wendt <dcrkid@yahoo.com>
2017-12-31 16:32:50 -0700

    Allow conform_bvm to work on MacOS

--- utilities/conform_bvm.sh
+++ utilities/conform_bvm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name \*.bvm | xargs -n 1 -P 16 python utilities/bvmfmt.py --symfile build/bugsite_alpha.sym --charmap script/charmap.txt
+find . -name \*.bvm | xargs -n 1 -P 16 utilities/find_python.sh utilities/bvmfmt.py --symfile build/bugsite_alpha.sym --charmap script/charmap.txt
